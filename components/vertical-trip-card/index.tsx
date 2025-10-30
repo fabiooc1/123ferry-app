ba6563316b55c745c197b1befa86b31366444e1f
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-30 18:31:11 -0300

    card de viagem  verticalizado

---
+++ components/vertical-trip-card/index.tsx
@@ -0,0 +1,43 @@
+import { colors } from "@/constants/colors";
+import { UsersIcon } from "phosphor-react-native";
+import { Text, View } from "react-native";
+import { s } from "./styles";
+
+type VerticalTripCardProps = {
+  departureHour: string;
+  arrivalHour: string;
+  ferry: {
+    name: string;
+    maxPeoplesCapacity: number;
+  };
+  amountPassengers: number;
+};
+
+export default function VerticalTripCard({
+  departureHour,
+  arrivalHour,
+  ferry,
+  amountPassengers,
+}: VerticalTripCardProps) {
+
+  console.log("ESTOU SENDO RENDERIZADO")
+  return (
+    // <Link href="/" asChild>
+      <View style={s.container}>
+        <View style={s.startContainer}>
+            <Text style={s.departureHour}>{departureHour}</Text>
+            <Text style={s.arrivalHour}>Chegada {arrivalHour}</Text>
+            <Text style={s.ferryName}>{ferry.name}</Text>
+        </View>
+
+        <View style={s.footerContainer}>
+          <Text style={s.footerLabel}>
+            {amountPassengers}/{ferry.maxPeoplesCapacity}
+          </Text>
+
+          <UsersIcon weight="fill" color={colors.icon.secondary} size={16} />
+        </View>
+      </View>
+    // </Link>
+  );
+}
