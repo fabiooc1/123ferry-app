4a0abbf13b9f094458b287a8610e439abd5cf48d
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-30 18:31:35 -0300

    cores da aplicacao

---
+++ constants/colors.ts
@@ -0,0 +1,23 @@
+export const colors = {
+    bg: {
+        primary: '#FFFFFF',
+        secondary: '#F5F7FA',
+        tertiary: '#F5F7FA'
+    },
+    text: {
+        primary: '#102A43',
+        secondary: '#486581',
+        tertiary: '#829AB1'
+    },
+    border: {
+        primary: '#E9EDF0'
+    },
+    icon: {
+        primary: '#00529B',
+        secondary: '#3B82F6',
+    },
+    status: {
+        avaliable: '#10B981',
+        error: "#EF4444"
+    }
+}
