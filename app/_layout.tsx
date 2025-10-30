8b01c5d11ac9d021db559363ff4c4de433551823
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-30 19:16:53 -0300

    carregamento de fonts

---
+++ app/_layout.tsx
@@ -0,0 +1,36 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  console.log("TESTANDO")
+  const [loaded, error] = useFonts({
+    "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+    "Inter-ExtraBold": require("../assets/fonts/Inter-ExtraBold.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
