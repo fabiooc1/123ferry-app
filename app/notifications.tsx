11dc55e9bd2af8115e3674c0b370306d94656a14
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-30 18:26:27 -0300

    iniciando pagina de notificacoes

---
+++ app/notifications.tsx
@@ -0,0 +1,13 @@
+import { StatusBar, Text, View } from "react-native";
+
+export default function NotificationsScreen() {
+    return (
+        <>
+        <View style={{ flex: 1 }}>
+            <Text>Notificacoes</Text>
+        </View>
+        
+        <StatusBar />
+        </>
+    )
+}
