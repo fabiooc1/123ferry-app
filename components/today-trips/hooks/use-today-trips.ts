fe43ff5c38f8a0eb596dde9957c1e1ff96860441
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-30 20:10:08 -0300

    melhorando hook

--- components/today-trips/hooks/use-today-trips.ts
+++ components/today-trips/hooks/use-today-trips.ts
@@ -3,31 +3,35 @@ import { tripService } from "@/services/tripService";
 import { useEffect, useState } from "react";
 
 export function useTodayTrips(routeId: number) {
+  const [isLoading, setIsLoading] = useState(true)
   const [tripsPaginationData, setTripsPaginationData] =
     useState<TripsPaginationModel | null>(null);
 
-  async function loadTickets() {
-    try {
-      setTripsPaginationData(null);
-      const currentTimestampUTC = new Date().toISOString();
-      const result = await tripService.getAll(
-        1,
-        3,
-        routeId,
-        currentTimestampUTC
-      );
-
-      setTripsPaginationData(result);
-    } catch (error) {
-      console.error("ERROR", error);
-    }
-  }
-
   useEffect(() => {
+    async function loadTickets() {
+      try {
+        setIsLoading(true)
+        const currentTimestampUTC = new Date().toISOString();
+        const resultData = await tripService.getAll(
+          1,
+          3,
+          routeId,
+          currentTimestampUTC
+        );
+
+        setTripsPaginationData(resultData);
+      } catch (error) {
+        console.error("ERROR", error);
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
     loadTickets();
-  }, []);
+  }, [routeId]);
 
   return {
     tripsPaginationData,
+    isLoading,
   };
 }
