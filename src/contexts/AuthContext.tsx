7105ee34b633c67db2f0c929835474ab44489905
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-31 17:14:41 -0300

    Contexto de autenticacao

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,47 @@
+import { UserModel } from "@/models/UserModel";
+import { userService } from "@/services/userService";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+type AuthContextProps = {
+  isLoadingUser: boolean;
+  user: UserModel | null;
+}
+
+const AuthContext = createContext({} as AuthContextProps)
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [isLoadingUser, setIsLoadingUser] = useState(true)
+  const [user, setUser] = useState<UserModel | null>(null)
+
+  useEffect(() => {
+    setIsLoadingUser(true)
+
+    userService.get()
+    .then(userData => {
+        setUser(userData)
+    })
+    .finally(() => {
+        setIsLoadingUser(false)
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ isLoadingUser, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+    const ctx = useContext(AuthContext)
+
+    if (!ctx) {
+        throw new Error("Use Auth must be used with AuthProvider")
+    }
+
+    return ctx
+}
