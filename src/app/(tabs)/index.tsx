94747b1a53d5f9b55b42af6679ce73cdb03a7116
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-31 21:33:33 -0300

    arrumando estilos da pagina

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,31 @@
 import Header from "@/components/header";
 import TodayTrips from "@/components/today-trips";
 import { colors } from "@/constants/colors";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={s.pageContainer}>
+      <Header />
+
+      <View style={s.pageContent}>
+        <Text style={s.pageTitle}>Passagens para hoje</Text>
+        
+        <TodayTrips title="São Luís -> Conjupe" routeId={1} />
+        <TodayTrips title="Conjupe -> São Luís" routeId={2} />
+      </View>
+    </SafeAreaView>
+  );
+}
 
 const s = StyleSheet.create({
   pageContainer: {
     flex: 1,
-    paddingTop: StatusBar.currentHeight || 0,
     backgroundColor: colors.bg.primary
   },
-  pageContentContainer: {
+  pageContent: {
+    flex: 1,
     gap: 12,
   },
   pageTitle: {
@@ -19,18 +35,3 @@ const s = StyleSheet.create({
     paddingLeft: 22
   },
 });
-
-export default function HomeScreen() {
-  return (
-    <View style={s.pageContainer}>
-      <Header />
-
-      <View style={s.pageContentContainer}>
-        <Text style={s.pageTitle}>Passagens para hoje</Text>
-        
-        <TodayTrips title="São Luís -> Conjupe" routeId={1} />
-        <TodayTrips title="Conjupe -> São Luís" routeId={2} />
-      </View>
-    </View>
-  );
-}
