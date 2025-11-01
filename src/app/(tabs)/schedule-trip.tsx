c9b4c2da4639b5026982f58f88c7da0f22ca3694
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-31 21:34:05 -0300

    pagina para listar viagens

---
+++ src/app/(tabs)/schedule-trip.tsx
@@ -0,0 +1,36 @@
+import ScheduleTripForm from "@/components/forms/schedule-trip-form";
+import Header from "@/components/header";
+import { colors } from "@/constants/colors";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ScheduleTripScreen() {
+  return (
+    <SafeAreaView style={s.pageContainer}>
+      <Header />
+
+      <View style={s.pageContent}>
+        <Text style={s.pageTitle}>Agende uma passagem</Text>
+        <ScheduleTripForm />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const s = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    backgroundColor: colors.bg.primary,
+  },
+  pageContent: {
+    flex: 1,
+    paddingHorizontal: 22,
+    paddingBottom: 10,
+    gap: 12,
+  },
+  pageTitle: {
+    color: colors.text.primary,
+    fontSize: 24,
+    fontWeight: "800",
+  }
+});
