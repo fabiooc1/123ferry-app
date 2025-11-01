898a48758c063ac0dae1efc95dd9023a6019682d
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-01 20:14:10 -0300

    melhorando componente

--- src/components/vertical-trip-card/index.tsx
+++ src/components/vertical-trip-card/index.tsx
@@ -1,41 +1,43 @@
 import { colors } from "@/constants/colors";
+import { TripModel } from "@/models/TripModel";
+import { getHours } from "@/utils/date";
+import { Link } from "expo-router";
 import { UsersIcon } from "phosphor-react-native";
 import { Text, View } from "react-native";
 import { s } from "./styles";
 
 type VerticalTripCardProps = {
-  departureHour: string;
-  arrivalHour: string;
-  ferry: {
-    name: string;
-    maxPeoplesCapacity: number;
-  };
-  amountPassengers: number;
+  trip: TripModel
 };
 
 export default function VerticalTripCard({
-  departureHour,
-  arrivalHour,
-  ferry,
-  amountPassengers,
+  trip
 }: VerticalTripCardProps) {
+  
   return (
-    // <Link href="/" asChild>
-    <View style={s.container}>
-      <View style={s.startContainer}>
-        <Text style={s.departureHour}>{departureHour}</Text>
-        <Text style={s.arrivalHour}>Chegada {arrivalHour}</Text>
-        <Text style={s.ferryName}>{ferry.name}</Text>
-      </View>
+    <Link
+      href={{
+        pathname: "/(tabs)/(schedule-trip)/schedule-details",
+        params: {
+          tripId: trip.id.toString()
+        }
+      }}
+    >
+      <View style={s.container}>
+        <View style={s.startContainer}>
+          <Text style={s.departureHour}>{getHours(trip.dataPartida)}</Text>
+          <Text style={s.arrivalHour}>Chegada {getHours(trip.dataChegada)}</Text>
+          <Text style={s.ferryName}>{trip.ferry.nome}</Text>
+        </View>
 
-      <View style={s.footerContainer}>
-        <Text style={s.footerLabel}>
-          {amountPassengers}/{ferry.maxPeoplesCapacity}
-        </Text>
+        <View style={s.footerContainer}>
+          <Text style={s.footerLabel}>
+            {trip.quantidadeDePassageiros}/{trip.ferry.maximoDePessoas}
+          </Text>
 
-        <UsersIcon weight="fill" color={colors.icon.secondary} size={16} />
+          <UsersIcon weight="fill" color={colors.icon.secondary} size={16} />
+        </View>
       </View>
-    </View>
-    // </Link>
+    </Link>
   );
 }
