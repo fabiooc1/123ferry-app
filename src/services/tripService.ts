957b489ec9e105e04597196ce5c6332addc7036a
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-01 20:13:22 -0300

    pegando detalhes da passagem e todos os tipos de pessoas

--- src/services/tripService.ts
+++ src/services/tripService.ts
@@ -1,4 +1,6 @@
 import { api } from "@/lib/axios"
+import { PassagerTypeModel } from "@/models/PassagerTypeModel"
+import { TripModel } from "@/models/TripModel"
 import { TripsPaginationModel } from "@/models/TripsPaginationModel"
 
 class TripService {
@@ -18,6 +20,34 @@ class TripService {
             }
 
             return response.data as TripsPaginationModel
+        }  catch {
+            throw new Error("Serviço fora do ar. Tente novamente mais tarde")
+        }
+    }
+
+    async get(tripId: bigint) {
+        try {
+            const response = await api.get(`/viagem/${tripId}`)
+
+            if (response.status !== 200) {
+                throw new Error("Não foi possível buscar essa viagem")
+            }
+
+            return response.data as TripModel
+        }  catch {
+            throw new Error("Serviço fora do ar. Tente novamente mais tarde")
+        }
+    }
+
+    async getAllPassagerTypes(): Promise<PassagerTypeModel[]> {
+        try {
+            const response = await api.get('/tipo-passageiro')
+
+            if (response.status !== 200) {
+                throw new Error("Não foi possível os tipos de passageiros")
+            }
+
+            return response.data as PassagerTypeModel[]
         }  catch {
             throw new Error("Serviço fora do ar. Tente novamente mais tarde")
         }
