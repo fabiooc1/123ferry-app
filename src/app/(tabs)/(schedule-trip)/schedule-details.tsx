ce738d73ce41c733aaf116cc32c454b00a65339d
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-02 13:45:42 -0300

    melhorando codigo e ui

--- src/app/(tabs)/(schedule-trip)/schedule-details.tsx
+++ src/app/(tabs)/(schedule-trip)/schedule-details.tsx
@@ -1,54 +1,85 @@
+import Button from "@/components/button";
 import Header from "@/components/header";
+import PageContentLoading from "@/components/page-content-loading";
 import TripHeader from "@/components/trip-header";
 import TripPassagersManager from "@/components/trip-passagers-manager";
+import TripVehiclesManager from "@/components/trip-vehicles-manager";
 import { colors } from "@/constants/colors";
-import { TripModel } from "@/models/TripModel";
-import { tripService } from "@/services/tripService";
+import { usePurchasePassager } from "@/contexts/PurshasePassagerContext";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { useEffect } from "react";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ScheduleDetailsScreen() {
   const { tripId }: { tripId: string } = useLocalSearchParams();
-
-  const [trip, setTrip] = useState<TripModel | null>(null);
-  const [isLoadingPageContent, setIsLoadingPageContent] = useState(true);
-
   const navigate = useRouter();
 
-  async function loadPageContent() {
-    try {
-      setIsLoadingPageContent(true);
-      const [tripData] = await Promise.all([tripService.get(BigInt(tripId))]);
+  const { loadDataForTrip, currentTrip, passagers, vehicles, isLoading } =
+    usePurchasePassager();
 
-      setTrip(tripData);
+  async function loadPageContent() {
+    if (!tripId) return;
+
+    try {
+      loadDataForTrip(tripId);
     } catch (error) {
       if (error instanceof Error) {
-        Alert.alert("Error", error.message);
+        Alert.alert("Erro ao carregar", error.message);
       }
 
       navigate.back();
-    } finally {
-      setIsLoadingPageContent(false);
     }
   }
 
   useEffect(() => {
     loadPageContent();
-  }, []);
+  }, [tripId]);
+
+  if (isLoading) {
+    return <PageContentLoading />;
+  }
+
+  const isValidPushUserToCheckoutPage = () => {
+    const totalPassagersInCart = passagers.length;
+    const driverCount = passagers.filter(
+      (passager) => passager.passagerTypeId === 1
+    ).length;
+    const vehicleCount = vehicles.length;
+
+    const isCartNotEmpty = totalPassagersInCart > 0;
+    const isDriverVehicleRatioCorrect = driverCount === vehicleCount;
+
+    return isCartNotEmpty && isDriverVehicleRatioCorrect;
+  };
+
+  function handleNavigateToCheckout() {
+    navigate.push("/(tabs)/(schedule-trip)/checkout");
+  }
 
   return (
     <SafeAreaView style={s.pageContainer}>
       <Header />
 
-      <View style={s.pageContent}>
-        {trip && (
+      <ScrollView
+        style={s.scrollContainer}
+        contentContainerStyle={s.scrollContentContainer}
+      >
+        {currentTrip && (
           <>
-            <TripHeader title="Detalhes da passagem" trip={trip} />
+            <TripHeader title="Detalhes da passagem" trip={currentTrip} />
             <TripPassagersManager />
+            <TripVehiclesManager />
           </>
         )}
+      </ScrollView>
+
+      <View style={s.footerContainer}>
+        <Button
+          label="Prosseguir"
+          onPress={handleNavigateToCheckout}
+          disabled={!isValidPushUserToCheckoutPage()}
+        />
       </View>
     </SafeAreaView>
   );
@@ -59,10 +90,18 @@ const s = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.bg.primary,
   },
-  pageContent: {
+  scrollContainer: {
     flex: 1,
+  },
+  scrollContentContainer: {
     paddingHorizontal: 22,
-    paddingBottom: 10,
+    paddingBottom: 20,
     gap: 26,
+  },
+  footerContainer: {
+    padding: 22,
+    backgroundColor: colors.bg.primary,
+    borderTopWidth: 1,
+    borderTopColor: colors.border.primary,
   },
 });
