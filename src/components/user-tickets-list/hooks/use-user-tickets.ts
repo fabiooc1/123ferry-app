040847282462581a21ff41488b6f7a6656d701de
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-02 17:42:22 -0300

    hook para facilitar o uso das passagens

---
+++ src/components/user-tickets-list/hooks/use-user-tickets.ts
@@ -0,0 +1,34 @@
+import { TicketsPaginationModel } from "@/models/TicketsPaginationModel";
+import { ticketService } from "@/services/ticketService";
+import { useEffect, useState } from "react";
+
+export function useUserTickets() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [ticketsPaginationData, setTicketsPaginationData] =
+    useState<TicketsPaginationModel | null>(null);
+
+  useEffect(() => {
+    async function loadTickets() {
+      try {
+        setIsLoading(true)
+        const resultData = await ticketService.getAll(
+          1,
+          10,
+        );
+
+        setTicketsPaginationData(resultData);
+      } catch (error) {
+        console.error("ERROR", error);
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    loadTickets();
+  }, []);
+
+  return {
+    ticketsPaginationData,
+    isLoading,
+  };
+}
