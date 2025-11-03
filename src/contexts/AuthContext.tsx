24670a315d7f9ac24b553bd4c41c45c4efbeaa5b
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-02 21:25:47 -0300

    corrigindo bug do 401 e adicionando funcao de logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,13 +1,19 @@
+import { jwtKeyPath } from "@/constants/security";
 import { UserModel } from "@/models/UserModel";
 import { LoginRequestDto } from "@/services/dtos/LoginRequestDto";
 import { userService } from "@/services/userService";
 import { AxiosError } from "axios";
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import { deleteItemAsync } from "expo-secure-store";
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from "react";
+
 
 type AuthContextProps = {
   isLoadingUser: boolean;
   user: UserModel | null;
+  setUser: Dispatch<SetStateAction<UserModel | null>>
   login: (data: LoginRequestDto) => Promise<void>
+  logout: () => Promise<void>
 }
 
 const AuthContext = createContext({} as AuthContextProps)
@@ -20,13 +26,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [isLoadingUser, setIsLoadingUser] = useState(true)
   const [user, setUser] = useState<UserModel | null>(null)
 
+  const navigate = useRouter()
+
   async function fetchUser() {
     try {
       const userData = await userService.get();
       setUser(userData);
     } catch (error) {
       if (error instanceof AxiosError) {
-        if (error.status === 401) {
+        if (error.response?.status === 401) {
+          await deleteItemAsync(jwtKeyPath)
           setUser(null);
           return;
         }
@@ -41,13 +50,19 @@ export function AuthProvider({ children }: AuthProviderProps) {
     await fetchUser();
   }
 
+  async function logout() {
+    await deleteItemAsync(jwtKeyPath)
+    setUser(null)
+    navigate.replace("/(auth)/login")
+  }
+
   useEffect(() => {
     setIsLoadingUser(true)
     fetchUser()
   }, [])
 
   return (
-    <AuthContext.Provider value={{ isLoadingUser, user, login }}>
+    <AuthContext.Provider value={{ isLoadingUser, user, login, setUser, logout }}>
       {children}
     </AuthContext.Provider>
   )
