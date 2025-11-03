162cc9e4bc55c3569bd912ff06f78658bcab5944
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-02 22:18:29 -0300

    implementacao futura, o atual esta gerando loop infinito

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,11 @@
-import { jwtKeyPath } from '@/constants/security';
-import axios from 'axios';
-import { router } from 'expo-router';
+import { jwtKeyPath } from "@/constants/security";
+import axios from "axios";
 import { getItem } from "expo-secure-store";
 
-
 export const api = axios.create({
-    baseURL: 'http://192.168.100.29:3000',
-    timeout: 8000 // 8 seconds
-})
+  baseURL: "http://192.168.100.29:3000",
+  timeout: 8000, // 8 seconds
+});
 
 api.interceptors.request.use((request) => {
   const token = getItem(jwtKeyPath);
@@ -19,13 +17,14 @@ api.interceptors.request.use((request) => {
   return request;
 });
 
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response && error.response.status === 401) {
-      router.replace('/login'); 
-    }
-    
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error: AxiosError) => {
+//     if (error.response && error.response.config.url !== "/auth/login" && error.response.status === 401) {
+//       await deleteItemAsync(jwtKeyPath)
+//       router.replace("/login");
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
