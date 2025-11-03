61ff6f49b0be120fab5f03a70d806de1c4375e54
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-03 12:21:58 -0300

    accordian de veiculos

---
+++ src/components/ticket-vehicles/vehicle-accordian/index.tsx
@@ -0,0 +1,37 @@
+import { colors } from "@/constants/colors";
+import { TicketVehicleModel } from "@/models/TicketVehicleModel";
+import { Accordion } from "@animatereactnative/accordion";
+import { CaretDownIcon } from "phosphor-react-native";
+import { Text, View } from "react-native";
+import { s } from "./styles";
+
+interface VehicleAccordionProps {
+  vehicle: TicketVehicleModel;
+}
+
+export default function VehicleAccordion({
+  vehicle,
+}: VehicleAccordionProps) {
+  return (
+    <Accordion.Accordion style={s.container}>
+      <Accordion.Header>
+        <View style={s.header}>
+          <Text style={s.headerTitle}>{vehicle.placa}</Text>
+
+          <Accordion.HeaderIcon>
+            <CaretDownIcon size={22} color={colors.text.secondary} />
+          </Accordion.HeaderIcon>
+        </View>
+      </Accordion.Header>
+
+      <Accordion.Expanded>
+        <View style={s.expandedContent}>
+          <View style={s.detailRow}>
+            <Text style={s.detailLabel}>Motorista</Text>
+            <Text style={s.detailValue}>{vehicle.passageiroId}</Text>
+          </View>
+        </View>
+      </Accordion.Expanded>
+    </Accordion.Accordion>
+  );
+}
---
+++ src/components/ticket-vehicles/vehicle-accordian/styles.ts
@@ -0,0 +1,44 @@
+import { colors } from "@/constants/colors";
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  container: {
+    backgroundColor: colors.bg.secondary,
+    borderColor: colors.border.primary,
+    borderWidth: 2,
+
+    borderRadius: 12,
+    padding: 10,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    color: colors.text.primary,
+    fontSize: 16,
+    fontFamily: "Inter-SemiBold",
+    flex: 1,
+    paddingRight: 10,
+  },
+  expandedContent: {
+    padding: 16,
+    gap: 12,
+  },
+  detailRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  detailLabel: {
+    color: colors.text.primary,
+    fontSize: 14,
+    fontFamily: "Inter-SemiBold",
+  },
+  detailValue: {
+    color: colors.text.secondary,
+    fontSize: 14,
+    fontFamily: "Inter-Regular",
+  },
+});
