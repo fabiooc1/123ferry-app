a19ce40e91649690ca276af3bed4d711b58330e4
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-03 12:21:41 -0300

    formatando data

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,14 +1,14 @@
 export function getHours(date: string): string {
-    const utcDate = new Date(date);
+  const utcDate = new Date(date);
 
   const options: Intl.DateTimeFormatOptions = {
-    timeZone: 'America/Sao_Paulo', 
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false 
+    timeZone: "America/Sao_Paulo",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
   };
 
-  return utcDate.toLocaleString('pt-BR', options);
+  return utcDate.toLocaleString("pt-BR", options);
 }
 
 export function formatDateTime(isoString: string) {
@@ -25,4 +25,25 @@ export function formatDateTime(isoString: string) {
   });
 
   return `${day}, ${month}. ${year} às ${time}`;
+}
+
+export function formatDate(isoString: string) {
+  if (!isoString) {
+    return "";
+  }
+
+  const date = new Date(isoString);
+
+  const day = date.getUTCDate();
+  const month = date.getUTCMonth() + 1;
+  const year = date.getUTCFullYear();
+
+  if (isNaN(day)) {
+    return "";
+  }
+
+  const formattedDay = String(day).padStart(2, "0");
+  const formattedMonth = String(month).padStart(2, "0");
+
+  return `${formattedDay}/${formattedMonth}/${year}`;
 }
