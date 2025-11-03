26583c682ecebb83861cfa5a4a14808747c25074
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-03 12:21:02 -0300

    listando os passageiros de uma viagem

---
+++ src/components/ticket-passagers/index.tsx
@@ -0,0 +1,36 @@
+import { colors } from "@/constants/colors";
+import { TicketPassagerModel } from "@/models/TicketPassagerModel";
+import { StyleSheet, Text, View } from "react-native";
+import PassagerAccordion from "./passager-accordion";
+
+interface TicketPassagersProps {
+    passagers: TicketPassagerModel[]
+}
+
+export default function TicketPassagers({ passagers }: TicketPassagersProps) {
+    return (
+        <View style={s.container}>
+            <Text style={s.title}>Passageiros ({passagers.length})</Text>
+
+            <View style={s.content}>
+                {passagers.map(passager => (
+                     <PassagerAccordion key={passager.cpf} passager={passager} />
+                ))}
+            </View>
+        </View>
+    )
+}
+
+const s = StyleSheet.create({
+    container: {
+        gap: 12
+    },
+    title: {
+        fontSize: 18,
+        fontFamily: "Inter-Bold",
+        color: colors.text.secondary
+    },
+    content: {
+        gap: 10
+    }
+})
---
+++ src/components/ticket-passagers/passager-accordion/index.tsx
@@ -0,0 +1,48 @@
+import { colors } from "@/constants/colors";
+import { TicketPassagerModel } from "@/models/TicketPassagerModel";
+import { formatDate } from "@/utils/date";
+import { Accordion } from "@animatereactnative/accordion";
+import { CaretDownIcon } from "phosphor-react-native";
+import { Text, View } from "react-native";
+import { s } from "./stylets";
+
+interface PassagerAccordionProps {
+  passager: TicketPassagerModel;
+}
+
+export default function PassagerAccordion({
+  passager,
+}: PassagerAccordionProps) {
+  return (
+    <Accordion.Accordion style={s.container}>
+      <Accordion.Header>
+        <View style={s.header}>
+          <Text style={s.headerTitle}>{passager.nomeCompleto}</Text>
+
+          <Accordion.HeaderIcon>
+            <CaretDownIcon size={22} color={colors.text.secondary} />
+          </Accordion.HeaderIcon>
+        </View>
+      </Accordion.Header>
+
+      <Accordion.Expanded>
+        <View style={s.expandedContent}>
+          <View style={s.detailRow}>
+            <Text style={s.detailLabel}>Tipo</Text>
+            <Text style={s.detailValue}>{passager.tipoId}</Text>
+          </View>
+
+          <View style={s.detailRow}>
+            <Text style={s.detailLabel}>CPF</Text>
+            <Text style={s.detailValue}>{passager.cpf}</Text>
+          </View>
+
+          <View style={s.detailRow}>
+            <Text style={s.detailLabel}>Data nascimento</Text>
+            <Text style={s.detailValue}>{formatDate(passager.dataNascimento)}</Text>
+          </View>
+        </View>
+      </Accordion.Expanded>
+    </Accordion.Accordion>
+  );
+}
