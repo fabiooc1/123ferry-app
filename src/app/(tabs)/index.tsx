33dd7b58f979d5cd18d4287bd84098cc4e559663
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-10 09:49:33 -0300

    estrategia de refresh

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,44 @@
 import Header from "@/components/header";
 import TodayTrips from "@/components/today-trips";
+import { useTodayTrips } from "@/components/today-trips/hooks/use-today-trips";
 import { colors } from "@/constants/colors";
-import { StyleSheet, Text, View } from "react-native";
+import { useCallback, useState } from "react";
+import { RefreshControl, StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const { isLoading: isLoadingRoute1, tripsPaginationData: tripsPaginationDataRoute1, loadTrips: refetchRoute1 } = useTodayTrips(1)
+  const { isLoading: isLoadingRoute2, tripsPaginationData: tripsPaginationDataRoute2, loadTrips: refetchRoute2 } = useTodayTrips(2)
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    Promise.all([
+      refetchRoute1,
+      refetchRoute2,
+    ])
+    .finally(() => {
+      setRefreshing(false);
+    });
+
+  }, [refetchRoute1, refetchRoute2]);
+
   return (
     <SafeAreaView style={s.pageContainer}>
       <Header />
 
-      <View style={s.pageContent}>
-        <Text style={s.pageTitle}>Passagens para hoje</Text>
-        
-        <TodayTrips title="São Luís -> Alcântara" routeId={1} />
-        <TodayTrips title="Alcântara -> São Luís" routeId={2} />
-      </View>
+      <ScrollView refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+        <View style={s.pageContent}>
+          <Text style={s.pageTitle}>Passagens para hoje</Text>
+
+          <TodayTrips routeName="São Luís -> Alcântara" isLoading={isLoadingRoute1} trips={tripsPaginationDataRoute1?.data || []} />
+          <TodayTrips routeName="Alcântara -> São Luís" isLoading={isLoadingRoute2} trips={tripsPaginationDataRoute2?.data || []} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
