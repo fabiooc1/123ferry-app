a4acef6cb35e44fa8a2b7a20a56232d57ccbd2b7
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-10 09:51:21 -0300

    alterando retorno de rota

--- src/models/TicketPaginationItem.ts
+++ src/models/TicketPaginationItem.ts
@@ -1,18 +1,11 @@
+import { RouteModel } from "./RouteModel";
+
 export interface TicketPaginationItem {
   id: number;
   codigo: string;
   viagem: {
     dataPartida: string;
     dataChegada: string;
-    rota: {
-      origem: {
-        id: number;
-        nome: string;
-      };
-      destino: {
-        id: number;
-        nome: string;
-      };
-    };
+    rota: RouteModel
   };
 }
