d8992e26257b98cc36be99bcc0475c8575256d64
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-10 09:50:29 -0300

    melhorando componente

--- src/components/today-trips/index.tsx
+++ src/components/today-trips/index.tsx
@@ -1,44 +1,51 @@
+import { TripModel } from "@/models/TripModel";
 import { FlatList, Text, View } from "react-native";
 import EmptyList from "../empty-list";
 import VerticalTripCard from "../vertical-trip-card";
-import VerticalTripCardSkeleton from "../vertical-trip-card/skeleton"; // Ajuste o caminho se necessário
-import { useTodayTrips } from "./hooks/use-today-trips";
+import VerticalTripCardSkeleton from "../vertical-trip-card/skeleton";
 import { s } from "./styles";
 
 type TodayTripsProps = {
-  title: string;
-  routeId: number;
+  routeName: string;
+  trips: TripModel[] | null;
+  isLoading: boolean;
 };
 
 const SKELETON_DATA = [1, 2, 3];
 
-export default function TodayTrips({ title, routeId }: TodayTripsProps) {
-  const { tripsPaginationData, isLoading } = useTodayTrips(routeId);
-
-  const renderRealItem = ({ item }: { item: any }) => <VerticalTripCard trip={item} />
+export default function TodayTrips({
+  routeName,
+  trips,
+  isLoading,
+}: TodayTripsProps) {
+  const renderRealItem = ({ item }: { item: TripModel }) => (
+    <VerticalTripCard trip={item} />
+  );
   const renderSkeletonItem = () => <VerticalTripCardSkeleton />;
 
   return (
     <View style={s.container}>
-      <Text style={s.title}>{title}</Text>
+      <Text style={s.title}>{routeName}</Text>
 
-      <FlatList
+      <FlatList<any>
         style={s.listContainer}
-        data={isLoading ? SKELETON_DATA : tripsPaginationData?.data}
+        data={isLoading ? SKELETON_DATA : trips}
         renderItem={isLoading ? renderSkeletonItem : renderRealItem}
         keyExtractor={(item, index) =>
-          isLoading ? `skeleton-${index}` : String(item.id)
+          isLoading ? `skeleton-${index}` : String((item as TripModel).id)
         }
         horizontal={true}
         ItemSeparatorComponent={() => <View style={{ width: 24 }} />}
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ paddingHorizontal: 22 }}
         ListEmptyComponent={
-          <EmptyList
-            title="Sem passagens"
-            description="Infelizmente não foi possível encontrar nenhuma passagem com essa rota"
-            width={360}
-          />
+          !isLoading && (!trips || trips.length === 0) ? (
+            <EmptyList
+              title="Sem passagens"
+              description="Infelizmente não foi possível encontrar nenhuma passagem com essa rota"
+              width={360}
+            />
+          ) : null
         }
       />
     </View>
