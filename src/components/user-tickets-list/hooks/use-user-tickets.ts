96cb2567db55394d877f526a180c97556e33c9c3
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-17 17:37:52 -0300

    aplicando loading na lista

--- src/components/user-tickets-list/hooks/use-user-tickets.ts
+++ src/components/user-tickets-list/hooks/use-user-tickets.ts
@@ -32,5 +32,6 @@ export function useUserTickets() {
   return {
     ticketsPaginationData,
     isLoading,
+    loadTickets,
   };
 }
--- src/components/user-tickets-list/index.tsx
+++ src/components/user-tickets-list/index.tsx
@@ -1,20 +1,32 @@
-import { View } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+import { useState } from "react";
+import { FlatList, RefreshControl, View } from "react-native";
 import EmptyList from "../empty-list";
 import TicketCard from "../ticket-card";
 import TicketCardSkeleton from "../ticket-card/skeleton";
 import { useUserTickets } from "./hooks/use-user-tickets";
 import { s } from "./styles";
 
-const SKELETON_DATA = [1, 2, 3, 4, 5]
+const SKELETON_DATA = [1, 2, 3, 4, 5];
 
 export default function UserTicketsList() {
-  const { isLoading, ticketsPaginationData } = useUserTickets()
+  const { isLoading, ticketsPaginationData, loadTickets } = useUserTickets();
+  const [refreshing, setRefreshing] = useState(false);
 
-  const renderRealItem = ({ item }: { item: any }) => <TicketCard ticket={item} />;
+  const renderRealItem = ({ item }: { item: any }) => (
+    <TicketCard ticket={item} />
+  );
 
   const renderSkeletonItem = () => <TicketCardSkeleton />;
-  
+
+  const onRefresh = async () => {
+    try {
+      setRefreshing(true);
+      await loadTickets();
+    } finally {
+      setRefreshing(false);
+    }
+  };
+
   return (
     <FlatList
       style={s.listContainer}
@@ -27,13 +39,16 @@ export default function UserTicketsList() {
       ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
       showsHorizontalScrollIndicator={false}
       ListEmptyComponent={
-        ticketsPaginationData && (
+        !isLoading ? (
           <EmptyList
             title="Sem passagens"
             description="Parece que você ainda não adquiriu nenhuma passagem, Adquira agora mesmo clicando no botão abaixo:"
             width={"100%"}
           />
-        )
+        ) : null
+      }
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }
     />
   );
